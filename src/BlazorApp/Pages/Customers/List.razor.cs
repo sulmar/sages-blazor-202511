f57d3f2aca3cb06a4f428d769476a4760b485a65
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-18 14:21:26 +0100

    Dodanie partial class

---
+++ src/BlazorApp/Pages/Customers/List.razor.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+
+namespace BlazorApp.Pages.Customers;
+
+public partial class List
+{
+    private IEnumerable<Customer> customers = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        customers = await Repository.GetAllAsync();
+    }
+}
