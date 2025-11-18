776fcdf50fdce04a0e21e21b631c944a37305e2f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-18 14:02:48 +0100

    Dodanie listy produktow

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -13,14 +13,21 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
 builder.Services.AddTransient<ICustomerRepository, InMemoryCustomerRepository>();
-
 builder.Services.AddTransient<Faker<Customer>, CustomerFaker>();
 
 builder.Services.AddTransient<IEnumerable<Customer>>(sp =>
 {
     return sp.GetRequiredService<Faker<Customer>>().Generate(100);
 });
+
+builder.Services.AddTransient<IProductRepository, InMemoryProductRepository>();
+builder.Services.AddTransient<Faker<Product>, ProductFaker>();
+
+builder.Services.AddTransient<IEnumerable<Product>>(sp =>
+{
+    return sp.GetRequiredService<Faker<Product>>().Generate(100);
+});
+
 
 await builder.Build().RunAsync();
