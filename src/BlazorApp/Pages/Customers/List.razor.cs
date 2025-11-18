a7aaa23c509cdf0fe07f1721d85904749b446fb4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-18 15:07:29 +0100

    refactor: Przeniesienie tabeli klientów do komponentu

--- src/BlazorApp/Pages/Customers/List.razor.cs
+++ src/BlazorApp/Pages/Customers/List.razor.cs
@@ -4,10 +4,5 @@ namespace BlazorApp.Pages.Customers;
 
 public partial class List
 {
-    private IEnumerable<Customer> customers = [];
-
-    protected override async Task OnInitializedAsync()
-    {
-        customers = await Repository.GetAllAsync();
-    }
+   
 }
