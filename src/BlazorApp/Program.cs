c229fa2af202caf4aa5b3fe6f9c9d730c20c180a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-11-18 12:30:38 +0100

    refactor: Wstrzykiwanie repozytorium

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -1,4 +1,7 @@
 using BlazorApp;
+using Domain.Abstractions;
+using Domain.Models;
+using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +10,18 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+builder.Services.AddTransient<ICustomerRepository, InMemoryCustomerRepository>();
+
+builder.Services.AddTransient<IEnumerable<Customer>>(sp =>
+{
+    return new List<Customer>
+    {
+        new Customer { Id = 1, Name = "Customer #1", Email = "john@domain.com" },
+        new Customer { Id = 2, Name = "Customer #2", Email = "kate@domain.com" },
+        new Customer { Id = 3, Name = "Customer #3", Email = "bart@domain.com" },
+    };
+});
 
 await builder.Build().RunAsync();
