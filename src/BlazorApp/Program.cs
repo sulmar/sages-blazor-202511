60268016a0fafa6359a93820fa0fb8ac43c8af74
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-12-09 12:06:29 +0100

    refactor: Przeniesienie HttpClient do uslugi

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp;
+using BlazorApp.Services;
 using Bogus;
 using Domain.Abstractions;
 using Domain.Models;
@@ -11,9 +12,15 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7039") });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7039") });
+
+
+// dotnet add package Microsoft.Extensions.Http
+builder.Services.AddHttpClient<ICustomerService, ApiCustomerService>(
+    client => client.BaseAddress = new Uri("https://localhost:7039"));
+
 
 
 await builder.Build().RunAsync();
---
+++ src/BlazorApp/Services/ApiCustomerService.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using System.Diagnostics;
+using System.Net.Http.Json;
+
+namespace BlazorApp.Services;
+
+public interface ICustomerService
+{
+    Task<IEnumerable<Customer>?> GetAll();
+}
+
+// Primary Constructor
+public class ApiCustomerService(HttpClient Http) : ICustomerService
+{
+    public Task<IEnumerable<Customer>?> GetAll() => Http.GetFromJsonAsync<IEnumerable<Customer>>("api/customers");
+}
